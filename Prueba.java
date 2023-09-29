51a768e1da966e432411a149ff75ccc21fd37536
digule <digule@gmail.com>
2023-09-29 13:23:53 +0200

    Apellios,Edad,Años,Comida

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,7 @@ public class Prueba{
     public static void main(String args[]){
         System.out.println("Gutierrez Lerones");
         System.out.println("19/2004");
+        System.out.println("Comida favorita: SecretoIberico");
+
     }
 }
