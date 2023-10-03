dcf7866ab09b5f13636747fe366dbe5e10051e92
digule <digule@gmail.com>
2023-10-03 13:26:47 +0200

    Años,Comida,Cantante

--- Prueba.java
+++ Prueba.java
@@ -1,8 +1,8 @@
-public class Prueba{
+    public class Prueba{
     public static void main(String args[]){
-        System.out.println("Gutierrez Lerones");
         System.out.println("19/2004");
         System.out.println("Comida favorita: SecretoIberico");
+        System.out.println("Cantante favorito: Melendi");
 
     }
 }
